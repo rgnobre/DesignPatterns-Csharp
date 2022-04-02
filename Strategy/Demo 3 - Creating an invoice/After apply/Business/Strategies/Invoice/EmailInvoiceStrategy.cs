e1ad9cf5e3daf96fd15a05cd4ecc4f0963c6dec4
rgnobre <rgnobre.s@gmail.com>
2022-04-02 18:11:34 +0100

    Demo 3 - Final

---
+++ Strategy/Demo 3 - Creating an invoice/After apply/Business/Strategies/Invoice/EmailInvoiceStrategy.cs
@@ -0,0 +1,29 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.Invoice
+{
+    public class EmailInvoiceStrategy : InvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            using (SmtpClient client = new SmtpClient("smtp.sendgrid.net", 587))
+            {
+                NetworkCredential credentials = new NetworkCredential("USERNAME", "PASSWORD");
+                client.Credentials = credentials;
+
+                MailMessage mail = new MailMessage("rgnobre@devnoobre.pt", "rgnobre@devnoobre.pt")
+                {
+                    Subject = "We've created an invoice for your order",
+                    Body = GenerateTextInvoice(order)
+            };
+
+                client.Send(mail);
+            }
+        }
+    }
+}
---
+++ Strategy/Demo 3 - Creating an invoice/After apply/Business/Strategies/Invoice/PrintOnDemandInvoiceStrategy.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Strategy_Pattern_First_Look.Business.Models;
+using Strategy_Pattern_First_Look.Business.Strategies.IStrategies;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.Invoice
+{
+    public class PrintOnDemandInvoiceStrategy : IInvoiceStrategy
+    {
+        public void Generate(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(order);
+
+                client.BaseAddress = new Uri("https://devnobre.pt");
+
+                client.PostAsync("/print-on-demand", new StringContent(content));
+            }
+        }
+    }
+}
