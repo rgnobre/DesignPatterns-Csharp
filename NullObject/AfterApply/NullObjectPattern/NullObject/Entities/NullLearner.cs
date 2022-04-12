417c27df0b5f39dede14470d89ae5fc578b953b0
rgnobre <rgnobre.s@gmail.com>
2022-04-12 20:49:11 +0100

    NullObject

---
+++ NullObject/AfterApply/NullObjectPattern/NullObject/Entities/NullLearner.cs
@@ -0,0 +1,11 @@
+﻿namespace NullObject.Entities
+{
+    public class NullLearner : ILearner
+    {
+        public int Id => -1;
+
+        public string UserName => "Just Browsing";
+
+        public int CoursesCompleted => 0;
+    }
+}
